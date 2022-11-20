6faf64904bdd51d1cb3affb46866fe763ded2825
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-20 22:56:34 +0100

    build add style to lib comp

--- projects/ngx-forms/src/lib/ngx-forms.component.ts
+++ projects/ngx-forms/src/lib/ngx-forms.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lib-ngx-forms',
   templateUrl: './ngx-forms.component.html',
-  styles: [
-  ]
+  styleUrls: ['./ngx-forms.component.scss']
 })
 export class NgxFormsComponent implements OnInit {
 
