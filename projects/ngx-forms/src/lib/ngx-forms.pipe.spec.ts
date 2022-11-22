22a6c6ba5bda7b6dc5e6b42423520f7d28f4ea66
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-22 09:00:28 +0100

    create pipe

---
+++ projects/ngx-forms/src/lib/ngx-forms.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NgxFormsPipe } from './ngx-forms.pipe';
+
+describe('NgxFormsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NgxFormsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
