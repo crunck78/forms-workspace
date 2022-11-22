fd71b9a4c68418b281d66476c710fea6bc346189
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-22 20:20:38 +0100

    build bind animation

--- projects/ngx-forms/src/lib/ngx-forms.component.ts
+++ projects/ngx-forms/src/lib/ngx-forms.component.ts
@@ -1,8 +1,15 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ApplyFormAnimations } from './ngx-form.animation';
+import { NgxFormsService } from './ngx-forms.service';
 
 @Component({
   selector: 'lib-ngx-forms',
   templateUrl: './ngx-forms.component.html',
   styleUrls: ['./ngx-forms.component.scss'],
+  animations: [ApplyFormAnimations]
 })
-export class NgxFormsComponent { }
+export class NgxFormsComponent {
+  constructor(public ngxs: NgxFormsService){
+
+  }
+}
