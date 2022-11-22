b3ae5a6ec44b3a7cd388495d27acd6641440d565
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-22 09:00:06 +0100

    build create directive

---
+++ projects/ngx-forms/src/lib/ngx-forms.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgxFormsDirective } from './ngx-forms.directive';
+
+describe('NgxFormsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NgxFormsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/ngx-forms/src/lib/ngx-forms.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[libNgxForms]'
+})
+export class NgxFormsDirective {
+
+  constructor() { }
+
+}
