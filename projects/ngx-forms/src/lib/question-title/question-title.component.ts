29327e3d71a5f016a796fdfc12edcfd7c9da1dcc
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-23 08:28:18 +0100

    build add question counter to title

--- projects/ngx-forms/src/lib/question-title/question-title.component.ts
+++ projects/ngx-forms/src/lib/question-title/question-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-question-title',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-title.component.scss']
 })
 export class QuestionTitleComponent implements OnInit {
+
+  @Input() id! : number;
 
   constructor() { }
 
