3f0fce3c11136f8869c9702afdb1c5b4c97a5809
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-26 23:19:18 +0100

    build single choice add choice

---
+++ projects/ngx-forms/src/lib/question-types/child-components/question-choice/question-choice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-question-choice',
+  templateUrl: './question-choice.component.html',
+  styleUrls: ['./question-choice.component.scss']
+})
+export class QuestionChoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
