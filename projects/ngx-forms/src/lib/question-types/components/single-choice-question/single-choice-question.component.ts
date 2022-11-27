846f182c039f2837c61024336865838e9571e84c
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-27 21:07:23 +0100

    update single-choice-question controler

--- projects/ngx-forms/src/lib/question-types/components/single-choice-question/single-choice-question.component.ts
+++ projects/ngx-forms/src/lib/question-types/components/single-choice-question/single-choice-question.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, ElementRef, forwardRef, OnInit, QueryList } from '@angular/core';
 import { FormArray, FormControl } from '@angular/forms';
 import { NgxFormsService } from '../../../ngx-forms.service';
+import { QuestionChoiceComponent } from '../../child-components/question-choice/question-choice.component';
 
 @Component({
   selector: 'lib-single-choice-question',
   templateUrl: './single-choice-question.component.html',
   styleUrls: ['./single-choice-question.component.scss']
 })
-export class SingleChoiceQuestionComponent implements OnInit {
+export class SingleChoiceQuestionComponent implements OnInit, AfterContentInit {
+
+  @ContentChildren(forwardRef(()=> QuestionChoiceComponent), {descendants: true}) questionChoices! : QueryList<QuestionChoiceComponent>;
 
   id!: number;
-  formArray = new FormArray<FormControl>([]);
+  //formArray = new FormArray<FormControl>([]);
+  formControl = new FormControl('');
 
   constructor(public ngxs: NgxFormsService) {
     this.id = this.ngxs.appendSection();
-    this.ngxs.appendControl(this.formArray);
+    this.ngxs.appendControl(this.formControl);
+
+  }
+
+  ngAfterContentInit(): void {
+    this.questionChoices.forEach(qc => qc.value$.subscribe(value => {
+      this.formControl.setValue(value);
+      this.unselectAll();
+      qc.color = 'primary';
+    }));
+  }
+
+  unselectAll(){
+    this.questionChoices.forEach(qc => qc.color = '');
   }
 
   ngOnInit(): void {
