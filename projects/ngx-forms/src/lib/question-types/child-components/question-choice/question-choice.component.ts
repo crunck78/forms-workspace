3e0ad231864db4bfa51863391882c72244652cb4
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-27 21:07:02 +0100

    update question-choice template and controler

--- projects/ngx-forms/src/lib/question-types/child-components/question-choice/question-choice.component.ts
+++ projects/ngx-forms/src/lib/question-types/child-components/question-choice/question-choice.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, ElementRef, forwardRef, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { QuestionChoiceValueDirective } from './question-choice-value.directive';
 
 @Component({
   selector: 'lib-question-choice',
   templateUrl: './question-choice.component.html',
   styleUrls: ['./question-choice.component.scss']
 })
-export class QuestionChoiceComponent implements OnInit {
+export class QuestionChoiceComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  value$ = new BehaviorSubject<string>('');
+  value! : string;
+  color! : string;
+
+  @ContentChild(forwardRef(()=> QuestionChoiceValueDirective) , {descendants: true}) valueDirective! : any;
+  constructor() {
+  }
+
+  ngAfterContentInit(): void {
+    this.value = this.valueDirective.el.nativeElement.innerText;
+    this.value$.next(this.value);
+  }
 
   ngOnInit(): void {
+  }
+
+  select(){
+    this.value$.next(this.value);
+    this.color = 'primary';
   }
 
 }
