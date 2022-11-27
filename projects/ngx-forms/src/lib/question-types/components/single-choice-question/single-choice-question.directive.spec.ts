567b3b9df581561072bc636e54a2aacdbfd37c52
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-27 21:05:04 +0100

    chore add single-choice directive

---
+++ projects/ngx-forms/src/lib/question-types/components/single-choice-question/single-choice-question.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SingleChoiceQuestionDirective } from './single-choice-question.directive';
+
+describe('SingleChoiceQuestionDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SingleChoiceQuestionDirective();
+    expect(directive).toBeTruthy();
+  });
+});
