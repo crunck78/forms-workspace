c7583b1ea4d095ba923f9d85c7701b88a5610ca6
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-29 19:35:52 +0100

    create email input

---
+++ projects/ngx-forms/src/lib/email-input-question/email-input-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-email-input-question',
+  templateUrl: './email-input-question.component.html',
+  styleUrls: ['./email-input-question.component.scss']
+})
+export class EmailInputQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
