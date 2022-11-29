66344daab77d1411721abe1b60fd585f21a1429d
mihaiandreineacsu <mihai@developerakademie.com>
2022-11-29 19:46:54 +0100

    update lib module imports

--- projects/ngx-forms/src/lib/ngx-forms.module.ts
+++ projects/ngx-forms/src/lib/ngx-forms.module.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Input, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { PtsDirective } from './pts.directive';
+
 import { NgxFormsComponent } from './ngx-forms.component';
 import { NoQuestionComponent } from './question-types/components/no-question/no-question.component';
 import { TextQuestionComponent } from './question-types/components/text-question/text-question.component';
 import { InputQuestionComponent } from './question-types/components/input-question/input-question.component';
-import { PtsDirective } from './pts.directive';
+
 import { QuestionHeaderComponent } from './question-types/child-components/question-header/question-header.component';
 import { QuestionFooterComponent } from './question-types/child-components/question-footer/question-footer.component';
 import { QuestionTitleComponent } from './question-types/child-components/question-title/question-title.component';
@@ -18,17 +22,19 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 import { NgxFormsDirective } from './ngx-forms.directive';
 import { NgxFormsPipe } from './ngx-forms.pipe';
 
-import { ReactiveFormsModule } from '@angular/forms';
 import { SingleChoiceQuestionComponent } from './question-types/components/single-choice-question/single-choice-question.component';
 import { QuestionChoiceComponent } from './question-types/child-components/question-choice/question-choice.component';
 import { SingleChoiceQuestionDirective } from './question-types/components/single-choice-question/single-choice-question.directive';
 import { QuestionChoiceValueDirective } from './question-types/child-components/question-choice/question-choice-value.directive';
 import { MultipleChoiceQuestionComponent } from './question-types/components/multiple-choice-question/multiple-choice-question.component';
 import { BooleanChoiceQuestionComponent } from './question-types/components/boolean-choice-question/boolean-choice-question.component';
+import { PhoneInputQuestionComponent } from './question-types/components/phone-input-question/phone-input-question.component';
+import { EmailInputQuestionComponent } from './question-types/components/email-input-question/email-input-question.component';
 
 
 
@@ -53,6 +59,8 @@ import { BooleanChoiceQuestionComponent } from './question-types/components/bool
     QuestionChoiceValueDirective,
     MultipleChoiceQuestionComponent,
     BooleanChoiceQuestionComponent,
+    PhoneInputQuestionComponent,
+    EmailInputQuestionComponent,
 
   ],
   imports: [
@@ -61,6 +69,7 @@ import { BooleanChoiceQuestionComponent } from './question-types/components/bool
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
+    MatSelectModule,
 
     CommonModule,
     FormsModule,
@@ -75,7 +84,9 @@ import { BooleanChoiceQuestionComponent } from './question-types/components/bool
     QuestionChoiceComponent,
     QuestionChoiceValueDirective,
     MultipleChoiceQuestionComponent,
-    BooleanChoiceQuestionComponent
+    BooleanChoiceQuestionComponent,
+    PhoneInputQuestionComponent,
+    EmailInputQuestionComponent
   ]
 })
 export class NgxFormsModule { }
