aa9cb8ee17b9616cbb3721386714916a9d5f4e8b
mihaiandreineacsu <mihai@developerakademie.com>
2022-12-01 08:17:32 +0100

    update submitter validation control

--- projects/ngx-forms/src/lib/question-types/child-components/question-submitter/question-submitter.component.ts
+++ projects/ngx-forms/src/lib/question-types/child-components/question-submitter/question-submitter.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormArray, FormControl } from '@angular/forms';
+import { NgxFormsService } from '../../../ngx-forms.service';
 
 @Component({
   selector: 'lib-question-submitter',
   templateUrl: './question-submitter.component.html',
   styleUrls: ['./question-submitter.component.scss']
 })
-export class QuestionSubmitterComponent implements OnInit {
+export class QuestionSubmitterComponent implements OnChanges {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public ngxs: NgxFormsService) { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
+
+  @Output() onOk = new EventEmitter();
+  @Input() ngxInput : FormControl | FormArray = new FormControl('');
+
+
 
 }
