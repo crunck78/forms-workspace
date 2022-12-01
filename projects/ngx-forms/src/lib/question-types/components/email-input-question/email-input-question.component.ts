6a3aedd586c7174b278b0403846395ec9a249664
mihaiandreineacsu <mihai@developerakademie.com>
2022-12-01 21:15:43 +0100

    email receives validators

--- projects/ngx-forms/src/lib/question-types/components/email-input-question/email-input-question.component.ts
+++ projects/ngx-forms/src/lib/question-types/components/email-input-question/email-input-question.component.ts
@@ -1,21 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { NgxFormsService } from '../../../ngx-forms.service';
+import { Base, returnProvider } from '../../../ngx-question.model';
 
 @Component({
   selector: 'lib-email-input-question',
   templateUrl: './email-input-question.component.html',
-  styleUrls: ['./email-input-question.component.scss']
+  styleUrls: ['./email-input-question.component.scss'],
+  providers: [returnProvider(EmailInputQuestionComponent)]
 })
-export class EmailInputQuestionComponent implements OnInit {
+export class EmailInputQuestionComponent implements OnInit, Base {
 
   id!: number;
-  input = new FormControl('',[ Validators.email, Validators.required]);
+  input = new FormControl('');
   @Input() placeholder: string  = "example@mail.com";
 
   constructor(public ngxs: NgxFormsService) {
     this.id = this.ngxs.appendSection();
-
   }
 
 
