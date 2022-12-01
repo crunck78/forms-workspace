40cefee303ce86de5a695d4734c4a752520075da
mihaiandreineacsu <mihai@developerakademie.com>
2022-12-01 21:15:59 +0100

    phone receive validators

--- projects/ngx-forms/src/lib/question-types/components/phone-input-question/phone-input-question.component.ts
+++ projects/ngx-forms/src/lib/question-types/components/phone-input-question/phone-input-question.component.ts
@@ -1,8 +1,9 @@
 
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormArray, FormControl, Validators } from '@angular/forms';
 import { MatSelectChange } from '@angular/material/select';
 import { NgxFormsService } from '../../../ngx-forms.service';
+import { Base, returnProvider } from '../../../ngx-question.model';
 import { COUNTRY_CODES } from './country-codes';
 
 
@@ -10,27 +11,42 @@ import { COUNTRY_CODES } from './country-codes';
 @Component({
   selector: 'lib-phone-input-question',
   templateUrl: './phone-input-question.component.html',
-  styleUrls: ['./phone-input-question.component.scss']
+  styleUrls: ['./phone-input-question.component.scss'],
+  providers: [returnProvider(PhoneInputQuestionComponent)]
 })
-export class PhoneInputQuestionComponent implements OnInit {
+export class PhoneInputQuestionComponent implements OnInit, Base {
 
   id!: number;
-  input = new FormControl('');
-  dialCode!: string;
+  input = new FormArray([
+    new FormControl('+49'),
+    new FormControl('', Validators.pattern(/^-?(0|[1-9]\d*)?$/))
+  ]);
   @Input() placeholder: string = "1234567890";
   readonly COUNTRY_CODES = COUNTRY_CODES;
 
   constructor(public ngxs: NgxFormsService) {
     this.id = this.ngxs.appendSection();
-
   }
 
   ngOnInit(): void {
     this.ngxs.appendControl(this.input);
   }
 
-  onCountryChange(event : MatSelectChange){
+  /**Value is written already in input field*/
+  handleInputPhoneNumber(event: InputEvent) {
+    let regexAllowedChars = /^-?(0|[1-9]\d*)?$/;
+    let inputField = event.target as HTMLInputElement;
+    let inputFieldValue = inputField.value;
 
+    let inputFieldValueAsArray = Array.from(inputFieldValue);
+
+    for (let index = 0; index < inputFieldValueAsArray.length; index++) {
+      const character = inputFieldValueAsArray[index];
+      if (!regexAllowedChars.test(character)) {
+        inputFieldValueAsArray[index] = '';
+      }
+    }
+    inputField.value = inputFieldValueAsArray.join('');
   }
 
 }
