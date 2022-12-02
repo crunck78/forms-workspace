ac8793b40e7fb68380103efe8f87a702ec625610
mihaiandreineacsu <mihai@developerakademie.com>
2022-12-02 19:53:01 +0100

    add validators to input

--- projects/ngx-forms/src/lib/question-types/components/input-question/input-question.component.ts
+++ projects/ngx-forms/src/lib/question-types/components/input-question/input-question.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { NgxFormsService } from '../../../ngx-forms.service';
+import { Base, returnProvider } from '../../../ngx-question.model';
 
 @Component({
   selector: 'lib-input-question',
   templateUrl: './input-question.component.html',
-  styleUrls: ['./input-question.component.scss']
+  styleUrls: ['./input-question.component.scss'],
+  providers: [returnProvider(InputQuestionComponent)]
 })
-export class InputQuestionComponent implements OnInit {
+export class InputQuestionComponent implements OnInit, Base {
 
   id!: number;
   input = new FormControl('');
@@ -17,7 +19,6 @@ export class InputQuestionComponent implements OnInit {
     this.id = this.ngxs.appendSection();
 
   }
-
 
   ngOnInit(): void {
     this.ngxs.appendControl(this.input);
