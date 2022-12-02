138aff375c8bdbef4352f532e8e238e05c531075
mihaiandreineacsu <mihai@developerakademie.com>
2022-12-02 19:54:06 +0100

    add validators to textarea

--- projects/ngx-forms/src/lib/question-types/components/text-question/text-question.component.ts
+++ projects/ngx-forms/src/lib/question-types/components/text-question/text-question.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { NgxFormsService } from '../../../ngx-forms.service';
+import { returnProvider } from '../../../ngx-question.model';
 
 
 @Component({
   selector: 'lib-text-question',
   templateUrl: './text-question.component.html',
-  styleUrls: ['./text-question.component.scss']
+  styleUrls: ['./text-question.component.scss'],
+  providers: [returnProvider(TextQuestionComponent)]
 })
 export class TextQuestionComponent implements OnInit {
 
   id!: number;
-  textArea = new FormControl('');
+  input = new FormControl('');
   @Input() placeholder: string = "Text";
 
   constructor(public ngxs: NgxFormsService) {
@@ -19,7 +21,7 @@ export class TextQuestionComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.ngxs.appendControl(this.textArea);
+    this.ngxs.appendControl(this.input);
   }
 
 }
